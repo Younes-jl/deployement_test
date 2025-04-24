3a3c1717c029ad2d176560e4dfabe8ab157ed658
M1cki0g <yassinetouli35@gmail.com>
2025-04-24 22:07:58 +0100

    update

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
    
   path('dashboard/', views.mon_dashboard, name='mon_dashboard'),
+  path('admin_events/', views.admin_events, name='admin_events'),
+  path('admin_users/', views.admin_users, name='admin_users'),
+  path('admin_participations/', views.admin_participations, name='admin_participations'),
 
 ]
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,7 +3,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from django.contrib.auth.models import User
 
-from crud_event.models import evenement
+from crud_event.models import evenement,participation
 
 @staff_member_required
 def mon_dashboard(request):
@@ -11,4 +11,20 @@ def mon_dashboard(request):
     nb_users = User.objects.count()
     nb_events = evenement.objects.count()
     return render(request, 'dashboard.html',{'nb_users':nb_users,'nb_events':nb_events})
+
+@staff_member_required
+def admin_events(request):
+    evenements = evenement.objects.all()
+    return render(request, 'admin_events.html', {'evenements': evenements})
+
+@staff_member_required
+def admin_users(request):
+    utilisateurs = User.objects.all()
+    return render(request, 'admin_users.html', {'utilisateurs': utilisateurs})
+
+@staff_member_required
+def admin_participations(request):
+    participations = participation.objects.all  # Use select_related for optimization
+    return render(request, 'admin_participations.html', {'participations': participations})
+
 
