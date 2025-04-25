d72267523dcb476232b5699ca158692a4affc9b6
Younes-jl <younesachlouj@gmail.com>
2025-04-25 13:08:23 +0200

    12222222222222222222

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -14,9 +14,9 @@ from .views import (
 urlpatterns = [
    
   path('dashboard/', views.mon_dashboard, name='mon_dashboard'),
-  path('admin_events/', views.admin_events, name='admin_events'),
-  path('admin_users/', views.admin_users, name='admin_users'),
-  path('admin_participations/', views.admin_participations, name='admin_participations'),
+   path('admin_events/', views.admin_events, name='admin_events'),
+   path('admin_users/', views.admin_users, name='admin_users'),
+   path('admin_participations/', views.admin_participations, name='admin_participations'),
 # Event URLs
     path('events/', EventListView.as_view(), name='event_list'),
     path('events/create/', EventCreateView.as_view(), name='event_create'),
