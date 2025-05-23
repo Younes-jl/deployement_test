a5ce91df181a3b8de8140e5831af4f611b307302
Younes-jl <younesachlouj@gmail.com>
2025-05-23 16:02:39 +0100

    Modification du fichier wsgi.py pour PythonAnywhere

--- main/wsgi.py
+++ main/wsgi.py
@@ -16,3 +16,17 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 application = get_wsgi_application() 
 
 # app = application
+
+import sys
+import os
+
+# Chemin vers le dossier contenant manage.py (ton projet principal)
+project_home = '/home/younes123/Event-management-final-version-'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Indiquer à Django où trouver les settings
+os.environ['DJANGO_SETTINGS_MODULE'] = 'main.settings'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
